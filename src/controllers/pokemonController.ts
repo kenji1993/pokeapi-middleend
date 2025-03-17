4450f5122f881f9ab52eaefe5ad17bbb1cb217b2
Kenji <kenji.arakaki93@gmail.com>
2025-03-17 20:37:42 -0300

    Adding typescript

--- src/controllers/pokemonController.ts
+++ src/controllers/pokemonController.ts
@@ -1,28 +1,39 @@
+import { Request, Response } from 'express';
 const axios = require('axios');
 
-exports.getPokemon = async (req, res) => {
+interface PokemonData {
+    name: string;
+    id: number;
+    types: string[];
+    weight: number;
+    height: number;
+    image: string;
+}
+
+
+exports.getPokemon = async (req: Request, res: Response) => {
     const { query } = req.params;
 
     try {
         const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${query}`);
-        
-        res.json({
+        const pokemonData: PokemonData = {
             name: data.name,
             id: data.id,
-            types: data.types.map((type) => type.type.name),
+            types: data.types.map((type: any) => type.type.name),
             weight: data.weight,
             height: data.height,
             image: data.sprites.front_default,
-        });
-    } catch (error) {
-        res.status(error.response?.status || 500).json({
-            error: error.response?.data || "Error al obtener el Pokémon",
+        }
+        res.json(pokemonData);
+    } catch (error: any) {
+        res.status(error.response.status || 500).json({
+            error: error.response.data || "Error al obtener el Pokémon",
         });
     }
 }
 
 
-exports.getPokemons = async (req, res) => {
+exports.getPokemons = async (req: Request, res: Response) => {
     const { limit = 10, offset = 0 } = req.query;
     try {
         const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
@@ -33,7 +44,7 @@ exports.getPokemons = async (req, res) => {
             previous: data.previous,
             results: data.results,
         });
-    } catch (error) {
+    } catch (error: any) {
         res.status(error.response?.status || 500).json({
             error: error.response?.data || "Error al obtener los Pokémon",
         });
